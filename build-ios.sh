c265aa9f96552fdb6e9564ab16568620b813c598
Akash Shah <44998751+itsalfredakku@users.noreply.github.com>
2024-09-28 11:45:17 +0530

    Update

--- build-ios.sh
+++ build-ios.sh
@@ -14,13 +14,13 @@ mkdir -p build
 cd build
 
 
-export IOS_TOOLCHAIN_FILE=${CURDIR}/../ios-cmake/ios.toolchain.cmake
+export IOS_TOOLCHAIN_FILE=${CURDIR}/ios-cmake/ios.toolchain.cmake
 
 
 declare -a PLATFORMS=("OS" "OS64" "SIMULATOR" "SIMULATOR64")
 PLATFORM="OS64"
-
-cmake ../SoftEtherVPN -G "Xcode" -DENABLE_BITCODE=0 \
+cmake ../SoftEtherVPN -G "Xcode" \
+    -DENABLE_BITCODE=0 \
     -DCMAKE_TOOLCHAIN_FILE=$IOS_TOOLCHAIN_FILE \
     -DPLATFORM=$PLATFORM \
     -DCMAKE_BUILD_TYPE=Debug \
@@ -29,8 +29,8 @@ cmake ../SoftEtherVPN -G "Xcode" -DENABLE_BITCODE=0 \
     -DLWS_WITHOUT_TESTAPPS=1 \
     -DCMAKE_INCLUDE_PATH=$(brew --prefix ncurses)/include \
     -DCMAKE_LIBRARY_PATH=$(brew --prefix ncurses)/lib \
-    -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR ../
-    -OPENSSL_ROOT_DIR=/Users/aksbju/Documents/GitHub/SoftEtherVPN-SDK/libcrypto
+    -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+    -DOPENSSL_ROOT_DIR=/Users/aksbju/Documents/GitHub/SoftEtherVPN-SDK/libcrypto
 
 
 cmake --build . --config Debug --target install
